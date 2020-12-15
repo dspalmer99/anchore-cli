952728e1a07bddd1ba6af9850afb88d33992c093
Daniel Palmer <dan.palmer@anchore.com>
2020-12-15 12:26:00 -0500

    Add unit tests to confirm subcommand help is not dependent on passing api auth.
    Signed-off-by: Daniel Palmer <dan.palmer@anchore.com>

--- tests/unit/cli/test_event.py
+++ tests/unit/cli/test_event.py
@@ -1,1 +1,19 @@
+import pytest
 from anchorecli.cli import event
+from click.testing import CliRunner
+
+
+class TestEventSubcommandHelp:
+    @pytest.mark.parametrize(
+        "subcommand, output_start",
+        [
+            (event.list, "Usage: list"),
+            (event.get, "Usage: get"),
+            (event.delete, "Usage: delete"),
+        ]
+    )
+    def test_event_subcommand_help(self, subcommand, output_start):
+        runner = CliRunner()
+        result = runner.invoke(subcommand, ["--help"])
+        assert result.exit_code == 0
+        assert result.output.startswith(output_start)
--- tests/unit/cli/test_system.py
+++ tests/unit/cli/test_system.py
@@ -1,1 +1,25 @@
+import pytest
 from anchorecli.cli import system
+from click.testing import CliRunner
+
+
+class TestSystemSubcommandHelp:
+    @pytest.mark.parametrize(
+        "subcommand, output_start",
+        [
+            (system.status, "Usage: status"),
+            (system.describe_errorcodes, "Usage: errorcodes"),
+            (system.wait, "Usage: wait"),
+            (system.delete, "Usage: del"),
+            (system.feeds, "Usage: feeds"),
+            (system.list, "Usage: list"),
+            (system.feedsync, "Usage: sync"),
+            (system.toggle_enabled, "Usage: config"),
+            (system.delete_data, "Usage: delete"),
+        ]
+    )
+    def test_event_subcommand_help(self, subcommand, output_start):
+        runner = CliRunner()
+        result = runner.invoke(subcommand, ["--help"])
+        assert result.exit_code == 0
+        assert result.output.startswith(output_start)
